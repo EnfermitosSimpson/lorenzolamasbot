83c0f8d0d2d2a9d786cfec40dee9f6a72a3e744d
EnfermitosSimpson <36039200+EnfermitosSimpson@users.noreply.github.com>
2018-02-01 12:48:07 -0300

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ CSE_KEY = "AIzaSyBpe97vYDqg_jeZUuNyFiH6KVsMWo2yZXc"
 CSE_CX = "000824544490182361845:y9fhnbyzq74"
 API_TOKEN = "491993570:AAEJCclpiXx0z6NjVlYorudpWxp-u7FIB6c"
 WEBHOOK_URL = "https://gisbotv1.herokuapp.com/"
-BATCH = 50
+BATCH = 10
 POLLING = False

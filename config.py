3cce8e7a4b820fb44c2c5f6355b5ba732aadd4b4
EnfermitosSimpson <36039200+EnfermitosSimpson@users.noreply.github.com>
2018-02-01 10:33:38 -0300

    Update and rename config_example.py to config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+BOT_NAME = "GISv1_bot"
+CSE_KEY = "AIzaSyBpe97vYDqg_jeZUuNyFiH6KVsMWo2yZXc"
+CSE_CX = "000824544490182361845:y9fhnbyzq74"
+API_TOKEN = "491993570:AAEJCclpiXx0z6NjVlYorudpWxp-u7FIB6c"
+WEBHOOK_URL = "https://gisbotv1.herokuapp.com/"
+BATCH = 50
+POLLING = False
--- config_example.py
+++
@@ -1,7 +0,0 @@
-BOT_NAME = ""
-CSE_KEY = ""
-CSE_CX = ""
-API_TOKEN = ""
-WEBHOOK_URL = ""
-BATCH = 10
-POLLING = True

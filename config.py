455e2e7e09b594eb6986cfe765c83bf4aef3a0c4
EnfermitosSimpson <36039200+EnfermitosSimpson@users.noreply.github.com>
2018-02-15 18:26:29 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+BOT_NAME = "gisearch_bot"
+CSE_KEY = "AIzaSyBpe97vYDqg_jeZUuNyFiH6KVsMWo2yZXc"
+CSE_CX = "000824544490182361845:y9fhnbyzq74"
+API_TOKEN = "512432533:AAFwjkNlQLs_5lBNFFWwjPv0OAa58juiS1s"
+WEBHOOK_URL = "https://gisearch.herokuapp.com"
+NGINX_SUBPATH = ""
+BATCH = 10
+POLLING = False
---
+++ config_example.py
@@ -0,0 +1,7 @@
+BOT_NAME = ""
+CSE_KEY = ""
+CSE_CX = ""
+API_TOKEN = ""
+WEBHOOK_URL = ""
+BATCH = 10
+POLLING = True
